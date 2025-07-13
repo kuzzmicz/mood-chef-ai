25746b442a85ca33d25ff1eb7783254b6e9f7f4c
kuzzmicz <kuzzmiczpies@gmail.com>
2025-07-13 14:38:23 +0200

    A simple client-side part of app is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
-
+import {React, useState} from 'react';
 function App() {
+  const [text, setText] = useState('');
+  const [recipe, setRecipe] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async() => {
+  setLoading(true);
+   try{
+  const response = await fetch('');
+  const data = await response.json(); 
+  setRecipe(data.recipe);
+   } catch (error) {
+  setRecipe("Error!")
+  }
+  setLoading(false);
+}
   return (
     <div className="App">
+      <h1>Mood Chef AI</h1>
+     <h2>Mood-Based Recipe Suggester</h2>
      
+     <input type ="text" 
+            value = {text}
+            onChange={(e)=> setText(e.target.value)}
+            placeholder="Type your sentence..."/>
+
+      <button onClick={handleSubmit}
+              disabled={loading}>{loading ? 'Analyzing...' : "Get a recommendation"} </button>
+                {recipe && (
+                  <div><h2>Recommendation:</h2>
+                  <p>{recipe}</p>
+                  </div>
+                )}
     </div>
   );
 }
